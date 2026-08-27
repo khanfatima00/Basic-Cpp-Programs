92a3f776252c837f402612e6ca674107f3ab16bc
fatimakhan00 <kfatima8888@gmail.com>
2026-08-27 10:37:28 +0530

    new

---
+++ rev_triangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+
+
+    for(int i=n;i>0;i--){
+        for(int j=i+1;j>0;j--){   
+         cout<<j<<" ";
+         
+        }
+       
+    cout<<endl;
+    }
+    return 0;
+}
