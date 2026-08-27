a4a237575d13702c852955034efcd2051d17510a
fatimakhan00 <kfatima8888@gmail.com>
2026-08-27 09:48:29 +0530

    new

---
+++ triangle_char.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+
+    char ch;
+    cout<<"Enter the desired character:";
+    cin>>ch;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<ch<<" ";
+            
+        }
+        ch++;
+     cout<<endl;
+    }
+
+   return 0;
+}
